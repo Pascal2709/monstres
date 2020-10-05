59949a08d49492ae7d38e076d97a47e3682d4fc9
Pascal2709 <pascal2709@gmail.com>
2020-10-05 15:31:14 +0200

    Updates

---
+++ precache-manifest.b408a448a9f243a5c3e8dd71ea32c917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c95e15d2042cb26ef62236d56797c6",
+    "url": "/monstres/index.html"
+  },
+  {
+    "revision": "7df06f777175d7c41689",
+    "url": "/monstres/static/css/main.36ed9744.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monstres/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstres/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df06f777175d7c41689",
+    "url": "/monstres/static/js/main.4916d3ba.chunk.js"
+  },
+  {
+    "revision": "a9551688f4ae865174fb",
+    "url": "/monstres/static/js/runtime-main.90cbb0c4.js"
+  }
+]);
